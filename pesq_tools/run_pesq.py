2d9bd4b9670467e17680771abd049da3883b3d58
Yusuf Cinar <cinar.yusuf@gmail.com>
2014-03-21 17:12:03 +0000

    pesq python wrapper

---
+++ pesq_tools/run_pesq.py
@@ -0,0 +1,24 @@
+import argparse
+import subprocess 
+
+def run_pesq(args):
+	pesq_command = "./pesq +16000 " + args.original_file + " " + args.recorded_file + " | grep \'P.862 Prediction (Raw MOS, MOS-LQO):\'"
+	print "pesq_command: ", pesq_command
+	#
+	subprocess.call([pesq_command], shell=True)
+
+def make_args_parser():
+	parser = argparse.ArgumentParser(version='%prog 0.1',
+					 description='''run_pesq: PESQ runner
+USAGE:
+<original_file> <degraded_file>
+									''')
+
+	parser.add_argument("original_file", help="original file path")
+	parser.add_argument("recorded_file", help="recorded file path")
+
+	return parser
+
+if __name__ == '__main__':
+	args = make_args_parser().parse_args()
+	run_pesq(args)
